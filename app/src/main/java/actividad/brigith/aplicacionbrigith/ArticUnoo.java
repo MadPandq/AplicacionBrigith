64e0902f828607a88c9fe5443a3943ac0ec9074c
Brigith <brigith.a.r.q@gmail.com>
2022-09-22 05:13:21 -0300

    Version cinco

--- app/src/main/java/actividad/brigith/aplicacionbrigith/ArticUnoo.java
+++ app/src/main/java/actividad/brigith/aplicacionbrigith/ArticUnoo.java
@@ -2,13 +2,25 @@ package actividad.brigith.aplicacionbrigith;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ArticUnoo extends AppCompatActivity {
 
+    ImageButton play_pause, prev, next;
+    MediaPlayer music;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artic_unoo);
+
+        play_pause= (ImageButton) findViewById(R.id.imgPlay);
+        prev= (ImageButton) findViewById(R.id.imgPrev);
+        next= (ImageButton) findViewById(R.id.imgNext);
     }
+
+
+
 }
