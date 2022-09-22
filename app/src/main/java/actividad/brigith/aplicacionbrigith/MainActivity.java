5b7e82004451404e5f0ea4a1eee1d3e4e6b42d0c
Brigith <brigith.a.r.q@gmail.com>
2022-09-22 06:54:14 -0300

    Version seis

--- app/src/main/java/actividad/brigith/aplicacionbrigith/MainActivity.java
+++ app/src/main/java/actividad/brigith/aplicacionbrigith/MainActivity.java
@@ -1,9 +1,15 @@
 package actividad.brigith.aplicacionbrigith;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +18,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text;
+    TextView text, text2, text3, text4, text5, text6;
+    private PendingIntent pendingIntent;
+    private final static String CHANNEL_ID = "NOTIFICACION";
+    private final static int NOTIFICACION_ID=0;
 
 
     @Override
@@ -21,46 +30,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         text= (TextView) findViewById(R.id.txtArticUno);
-        text= (TextView) findViewById(R.id.txtArticDos);
-        text= (TextView) findViewById(R.id.txtTheUno);
-        text= (TextView) findViewById(R.id.txtThe2);
-        text= (TextView) findViewById(R.id.txtChaseUno);
-        text= (TextView) findViewById(R.id.txtChaseDos);
+        text2= (TextView) findViewById(R.id.txtArticDos);
+        text3= (TextView) findViewById(R.id.txtTheUno);
+        text4= (TextView) findViewById(R.id.txtThe2);
+        text5= (TextView) findViewById(R.id.txtChaseUno);
+        text6= (TextView) findViewById(R.id.txtChaseDos);
 
 
 
         text.setOnClickListener(view -> {
-            Intent i = new Intent(MainActivity.this, ArticUnoo.class);
-            startActivity(i);
+            Intent e = new Intent(MainActivity.this, ArticUnoo.class);
+            startActivity(e);
         });
 
-        text.setOnClickListener(view -> {
-            Intent i = new Intent(MainActivity.this, ArticDoos.class);
-            startActivity(i);
+
+
+        text2.setOnClickListener(view -> {
+            Intent d = new Intent(MainActivity.this, ArticDoos.class);
+            startActivity(d);
         });
 
-        text.setOnClickListener(view -> {
-            Intent i = new Intent(MainActivity.this, TheUnoo.class);
-            startActivity(i);
+        text3.setOnClickListener(view -> {
+            Intent c = new Intent(MainActivity.this, TheUnoo.class);
+            startActivity(c);
         });
 
-        text.setOnClickListener(view -> {
-            Intent i = new Intent(MainActivity.this, TheDoos.class);
-            startActivity(i);
+        text4.setOnClickListener(view -> {
+            Intent b = new Intent(MainActivity.this, TheDoos.class);
+            startActivity(b);
         });
 
-        text.setOnClickListener(view -> {
-            Intent i = new Intent(MainActivity.this, ChaseUnoo.class);
-            startActivity(i);
+        text5.setOnClickListener(view -> {
+            Intent a = new Intent(MainActivity.this, ChaseUnoo.class);
+            startActivity(a);
         });
 
-        text.setOnClickListener(view -> {
+        text6.setOnClickListener(view -> {
             Intent i = new Intent(MainActivity.this, ChaseDoos.class);
             startActivity(i);
         });
     }
-
-
 
 }
 
